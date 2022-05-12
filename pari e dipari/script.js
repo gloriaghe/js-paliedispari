bee37ad924cddac63a9d3c176f4409763f21ccad
Gloria <gloghe@yahoo.it>
2022-05-12 15:50:29 +0200

    pari e dispari semplificato

--- pari e dipari/script.js
+++ pari e dipari/script.js
@@ -17,15 +17,35 @@ let somma = numGenerato + numeroUser;
 //definisco se somma pari o dispari
 parDispNormalizzato = pariDispariUser.toLowerCase()
 
+
+
 function pariDispari(somma) {
-    if (somma % 2 === 0 && parDispNormalizzato === "pari"){
-        return "La somma è pari, complimenti hai vinto.";
-    } else if (somma % 2 !== 0 && parDispNormalizzato === "dispari") { 
-        return "La somma è dispari, complimenti hai vinto.";
-    } else if (somma % 2 !== 0 && parDispNormalizzato === "pari"){
-        return "La somma è dispari... Mi dispiace ma hai perso.";
-    } else if (somma % 2 === 0 && parDispNormalizzato === "dispari") {
-        return "La somma è pari... Mi dispiace ma hai perso.";
+
+     if (somma % 2 === 0 ){
+         return "pari";
+     } else { 
+         return "dispari";
+     }
+}
+    
+function vinto() {
+
+    if (pariDispari(somma) === parDispNormalizzato) {
+        //immagine
+        const noneclass = document.getElementById("imgVinto").classList;
+        noneclass.remove("none");
+
+        
+        return "Hai vinto complimenti!!!";
+
+    } else {
+        //immagine
+        const noneclass2 = document.getElementById("imgPerso").classList;
+        noneclass2.remove("none");
+
+
+        return "Hai perso... Mi dispiace!";
+
     }
 }
 
@@ -33,15 +53,6 @@ function pariDispari(somma) {
 let numUserHTML = document.querySelector("#numeroscelto").innerHTML = "Hai scelto " + parDispNormalizzato + " e il numero " + numeroUser;
 let NumPCHTML = document.querySelector("#numeroPc").innerHTML = "Il Pc ha scelto il numero Random: " + numGenerato;
 let sommaHTML = document.querySelector("#somma").innerHTML = "La somma totale è: " + somma;
-let risultato = document.querySelector("#PersooVinto").innerHTML = pariDispari(somma);
-
-//BONUS
+let risultato = document.querySelector("#PersooVinto").innerHTML = vinto();
 
 
-    if ((somma % 2 === 0 && parDispNormalizzato === "pari") || (somma % 2 !== 0 && parDispNormalizzato === "dispari")) {
-        const noneclass = document.getElementById("imgVinto").classList;
-        noneclass.remove("none");
-    } else if ((somma % 2 !== 0 && parDispNormalizzato === "pari") || (somma % 2 === 0 && parDispNormalizzato === "dispari")) {
-        const noneclass2 = document.getElementById("imgPerso").classList;
-        noneclass2.remove("none");
-    }
